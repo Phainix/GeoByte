df23c1287041dfad2e3cbde135da7b15830a3cb4
Adekogbe Faith <28295747+Phainix@users.noreply.github.com>
2021-01-13 16:32:51 +0100

    Angular Skeleton

--- geo-byte-frontend/src/app/app.module.ts
+++ geo-byte-frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LocationsComponent } from './locations/locations.component';
+import { RouteComponent } from './route/route.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    SignUpComponent,
+    LocationsComponent,
+    RouteComponent
   ],
   imports: [
     BrowserModule,
